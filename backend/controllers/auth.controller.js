7cc81948bb5632b992c586d2b2cddaadf9b285b9
Raaj Patkar <raajpatkar@gmail.com>
2025-04-13 21:24:16 +0530

    hope so env file is removed

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,26 @@
+import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js"; // Ensure User is imported
 export const join= async (req, res) => {
+    try {
+        const{email,password,name} = req.body;
+        if(!email || !password || !name){
+          throw new Error("Please fill all the fields");
+        }
+        if(password.length < 6){
+            return res.status(400).json({message:"Password must be at least 6 characters"});
+        }
+        const userAlreadyexists = await User.findOne({ email });
+        if (userAlreadyexists) {
+            return res.status(400).json({success:false ,message:"User already exists"});
+        }
+        const hashedpassword = await bcryptjs.hash(password, 10);
+        
+        
+    } catch (error) {
+        console.log(error);
+         return res.status(400).json({success:false ,message:error.message});
+        
+    }
     res.send("Joining...");
 }
 export const access  = async (req, res) => {
--- backend/index.js
+++ backend/index.js
@@ -5,14 +5,16 @@ import authroutes from "./routes/authroutes.js";
 import { connectDatabase } from "./database/connectDB.js";
 dotenv.config();
 const app = express();
+const port = process.env.PORT || 5000;
 app.get("/", (req, res) => {
   
   res.send("Jai Shri raam");
 });
+app.use(express.json());
 app.use("/api/auth",authroutes)
-app.listen(3000, () => {
+app.listen(port, () => {
   connectDatabase();
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port ",port);
 });
 
 //ohHaSIsm9FEDMoyP
